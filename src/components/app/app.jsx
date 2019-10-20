ab60e992b63096559989a567e33d86683e754f9b
Sergey Shershnev <shv.sergey70@gmail.com>
2019-10-20 10:25:49 +0300

    added card component & added propTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import {MainPage} from "../main-page/main-page";
 import PropTypes from 'prop-types';
 
-export const App = () => {
-  return <MainPage/>;
+export const App = (props) => {
+  const {offers} = props;
+
+  return <MainPage
+    offers = {offers}
+  />;
+};
+
+App.propTypes = {
+  offers: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.oneOf([`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`])
+      })
+  )
 };
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {App} from "./components/app/app";
 
+const offersData = [
+  {
+    title: `Beautiful & luxurious apartment at great location`
+  },
+  {
+    title: `Wood and stone place`
+  },
+  {
+    title: `Canal View Prinsengracht`
+  },
+  {
+    title: `Nice, cozy, warm big bed apartment`
+  }
+];
+
 const init = () => {
   ReactDom.render(
-      <App />,
+      <App
+        offers = {offersData}
+      />,
       document.querySelector(`#root`)
   );
 };
