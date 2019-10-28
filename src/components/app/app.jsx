670ccd1d48f3a497f0264fe9638fa72af36e21a4
Sergey Shershnev <shv.sergey70@gmail.com>
2019-10-28 10:31:21 +0300

    tried to create routing & created component for offer detail

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import {MainPage} from "../main-page/main-page";
 import PropTypes from 'prop-types';
 import {offerPropTypes} from "../card/card";
+import {OfferDetail} from "../offer-detail/offer-detail";
+
+const getPageScreen = (props) => {
+  console.log(location.pathname);
+  console.log(/^offer\/\d$/.test(location.pathname));
+  // switch(location.pathname) {
+  //   case `/`:
+      return <MainPage offers = {props.firstPageProp} />;
+  //   case `/offer/:id`:
+  //     return <OfferDetail offer={props.secondPageProp} />;
+  // }
+};
 
 export const App = (props) => {
   const {offers} = props;
 
-  return <MainPage
-    offers = {offers}
-  />;
+  return <React.Fragment>{getPageScreen(props)}</React.Fragment>;
+  // return <MainPage
+  //   offers = {offers}
+  // />;
 };
 
 App.propTypes = {
