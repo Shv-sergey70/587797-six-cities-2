bf3c1319344fa1a14b0f7682115fa121489407de
Sergey Shershnev <shv.sergey70@gmail.com>
2019-12-01 16:43:49 +0300

    added dependencies & connected to store middleware

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {reducer} from "./reducer";
 import {Provider} from 'react-redux';
 import {App} from "./components/app/app";
 import {offersData} from "./mocks/offers";
+import configureAPI from "./api";
+import {compose} from "recompose";
+import thunk from "redux-thunk";
 
 const init = () => {
-  const store = createStore(reducer);
+  const api = configureAPI((...args) => store.dispatch(...args));
+
+  const store = createStore(reducer, compose(
+      applyMiddleware(thunk.withExtraArgument(api)),
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+  ));
 
   ReactDom.render(
       <Provider store={store}>
