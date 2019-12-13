6675a941ab7f0c8a513394880bc78337a4aee5bd
Sergey Shershnev <shv.sergey70@gmail.com>
2019-12-13 09:08:12 +0300

    added test for sort component

---
+++ src/components/sort/sort.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Sort} from "./sort";
+
+describe(`Sort section renders`, () => {
+  it(`Sort section is open`, () => {
+    const tree = renderer.create(<Sort
+      isOpen={true}
+      onOpenStateChange={jest.fn()}
+      activeSortingType={{
+        name: `POPULAR`,
+        text: `Popular`
+      }}
+      changeSortingType={jest.fn()}
+    />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`Sort section is closed`, () => {
+    const tree = renderer.create(<Sort
+      isOpen={false}
+      onOpenStateChange={jest.fn()}
+      activeSortingType={{
+        name: `PRICE_HIGH_TO_LOW`,
+        text: `Price: high to low`
+      }}
+      changeSortingType={jest.fn()}
+    />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
