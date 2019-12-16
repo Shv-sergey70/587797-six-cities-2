0cb28855dd4a2a091956058938c6fc40ea55fac3
Sergey Shershnev <shv.sergey70@gmail.com>
2019-12-16 08:42:55 +0300

    added const for http response

--- src/api.jsx
+++ src/api.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {ApiUrl, Route} from "./const/routes";
 import history from "./history";
 import {isPrivateRoute} from "./utils";
+import {HttpAnswerCode} from "./const/http-answer-codes";
 
 const configureAPI = () => {
   const api = axios.create({
@@ -13,7 +14,7 @@ const configureAPI = () => {
   const onSuccess = (response) => response;
 
   const onFail = (err) => {
-    if ((err.status === 401 || (err.response && err.response.status === 401)) && isPrivateRoute(err.config.method, err.config.url)) {
+    if ((err.status === HttpAnswerCode.UNAUTHORIZED || (err.response && err.response.status === HttpAnswerCode.UNAUTHORIZED)) && isPrivateRoute(err.config.method, err.config.url)) {
       history.push(Route.LOGIN);
     }
   };
---
+++ src/const/http-answer-codes.js
@@ -0,0 +1,3 @@
+export const HttpAnswerCode = {
+  UNAUTHORIZED: 401
+};
